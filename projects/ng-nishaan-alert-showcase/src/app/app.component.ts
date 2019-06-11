590521901b62b6bd81d249e11a7528fa254288d0
Gurpreet Deol <deol.gs@hotmail.com>
2019-06-11 12:11:48 -0700

    update alert library

--- projects/ng-nishaan-alert-showcase/src/app/app.component.ts
+++ projects/ng-nishaan-alert-showcase/src/app/app.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
+import { NgNishaanAlertService } from '../../../ng-nishaan-alert/src/lib/ng-nishaan-alert.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  //templateUrl: './app.component.html',
+  template: `
+    <ng-alert></ng-alert>
+    <button class="success" (click)="createSuccessMessage()">Create success alert</button>
+    <button class="danger" (click)="createDangerMessage()">Create danger alert</button>
+    <button class="warning" (click)="createWarningMessage()">Create warning alert</button>
+    <button class="info" (click)="createInfoMessage()">Create info alert</button>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'ng-nishaan-alert-showcase';
+
+  constructor(private alertService: NgNishaanAlertService){
+
+  }
+
+  public createSuccessMessage(): void {
+    this.alertService.createSuccessAlert('Sample success message');
+  }
+
+  public createDangerMessage(): void {
+    this.alertService.createDangerAlert('Sample danger message');
+  }
+
+  public createWarningMessage(): void {
+    this.alertService.createWarningAlert('Sample warning message');
+  }
+
+  public createInfoMessage(): void {
+this.alertService.createInfoAlert('Sample info message');
+  }
+
 }
